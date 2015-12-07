451e4f7a16866293ca2ab025b3b306b323597c9a
alex-B-Ly <abl12582@gmail.com>
2015-12-07 07:07:31 -0500

    added fadein for repo list, as well as working repo select function

--- js/functionality.js
+++ js/functionality.js
@@ -1,15 +1,40 @@
 $(document).ready(function() {
 
+
+
+// REPO SECTION
+
+	// FETCH REPO LIST ON BUTTON CLICK
 	function repoFetch(){
 		$('#repo-fetch').on('click', function(e) {
 			e.preventDefault();
 			$(this).fadeOut(800, function() {
-				$('.repo-title').css('display', 'block');
+				$('.repo-title').fadeIn(2000);
+				$('.repo-instructions').fadeIn(2000);
+				$('.repo-list').fadeIn(3000);
 			});
 		});
 	}
 
+	// SELECT REPO AND HIDE OTHERS
+	function repoSelect(){
+		$(document).on('click', '.repo-name', function(e) {
+			e.preventDefault();
+
+			if(!($(this).hasClass('selected-repo'))){
+				$(this).addClass('selected-repo');
+				$(this).siblings().addClass('hide-repo');
+			}else{
+				$(this).removeClass('selected-repo');
+				$(this).siblings().removeClass('hide-repo');
+			}
+		
+		});
+	}
+
+
 	//FUNCTIONS CALLED
 	repoFetch();
+	repoSelect();
 
 });
